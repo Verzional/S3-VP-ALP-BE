ed6c0ede7849480cf20786260795224f2fae381d
Verzional <verzional777@gmail.com>
2024-12-23 20:56:09 +0700

    Like and Comment

---
+++ src/model/CommentModel.ts
@@ -0,0 +1,21 @@
+import { Comment } from "@prisma/client";
+
+export interface CommentResponse {
+  id: number;
+  content: string;
+  userId: number;
+  postId: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export function toCommentResponse(comment: Comment): CommentResponse {
+  return {
+    id: comment.id,
+    content: comment.content,
+    userId: comment.userId,
+    postId: comment.postId,
+    createdAt: comment.createdAt,
+    updatedAt: comment.updatedAt,
+  };
+}
---
+++ src/model/LikeModel.ts
@@ -0,0 +1,15 @@
+import { Like } from "@prisma/client";
+
+export interface LikeResponse {
+  id: number;
+  userId: number;
+  postId: number;
+}
+
+export function toLikeResponse(like: Like): LikeResponse {
+  return {
+    id: like.id,
+    userId: like.userId,
+    postId: like.postId,
+  };
+}
