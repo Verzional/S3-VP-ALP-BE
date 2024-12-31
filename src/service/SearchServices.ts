468b52386e519d666326151c52118ae4ec75f7db
joeyssn1 <joeysonsalam01@student.ciputra.ac.id>
2024-12-31 23:01:40 +0700

    commit

--- src/service/SearchServices.ts
+++ src/service/SearchServices.ts
@@ -1,20 +1,22 @@
-import { SearchUser } from '../model/UserModel';
+import { UserProfile, getAllUsers } from '../model/UserModel'; 
 import { CommunityModel } from '../model/CommunityModel';
 
 class SearchServices {
-    async findUserByUsername(username: string): Promise<SearchUser | null> {
+    async findUserByUsername(username: string): Promise<UserProfile | null> {
         try {
-            const user = await SearchUser.findOne({ username });
+            const users = await getAllUsers();
+            const user = users.find(user => user.username === username) || null;
             return user;
         } catch (error) {
             console.error('Error finding user by username:', error);
             return null;
+
         }
     }
 
     async findCommunityByName(name: string): Promise<CommunityModel | null> {
         try {
-            const community = await CommunityModel.find({ name }).limit(1).exec();
+            const community = await CommunityModel.findOne({ name });
             return community;
         } catch (error) {
             console.error('Error finding community by name:', error);
